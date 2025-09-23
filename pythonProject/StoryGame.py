3e20ce410ba7aa3bad8c2c85c61e366766c22094
Kimsangwon-093 <akg9380@daum.net>
2025-09-24 02:49:21 +0900

    StoryGame 250924

---
+++ pythonProject/StoryGame.py
@@ -0,0 +1,21 @@
+# 단어 채우기 게임 Project
+# 3.project 완성!
+print("안녕하세요! 다음 질문에 답하고 이야기를 완성하세요.")
+print()
+animal = "이구아나"
+name = "스파이크"
+adjective1 = "길"
+adjective2 = "느릿느릿"
+adjective3 = "작은"
+color1 = "초록색"
+color2 = "노란색"
+noun1 = "나뭇잎"
+noun2 = "꽃"
+noun3 = "과일"
+noun4 = "공"
+
+
+print("저의 반려동물은 이구아나이며 이름은 스파이크 입니다.")
+print("길고 초록색이며 느릿느릿 합니다.")
+print("스파이크는 나뭇잎, 꽃, 과일을 먹습니다.");
+print("가장 좋아하는 장난감은 작은 노란색 공입니다.");
